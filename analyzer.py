8e45cfdbdb36e3724549332163037675fbf656e7
Stachq1 <stanislaw.piasecki@inf.ethz.ch>
2022-12-16 21:14:01 +0100

    Implement helper messages for command line interface. Implement initial check_properties function to check for car velocity and lap increments.

--- analyzer.py
+++ analyzer.py
@@ -12,22 +12,21 @@ def arg_parser():
     parser = argparse.ArgumentParser(description="Rosbag Analyzer", epilog="If you have propositions for other flags,\nfeel free to create an issue on GitHub!\nMaintainer: S.Piasecki")
 
     # Add the different parser arguments
-    # TODO: Add help messages!
-    parser.add_argument('--per', action='store_true', help="")
-    parser.add_argument('--est', action='store_true', help="")
-    parser.add_argument('--con', action='store_true', help="")
+    parser.add_argument('--per', action='store_true', help="Filter for rosbags where perception runs at ~10 Hz")
+    parser.add_argument('--est', action='store_true', help="Filter for rosbags where estimation runs at ~10 Hz")
+    parser.add_argument('--con', action='store_true', help="Filter for rosgags where control runs at ~10 Hz")
 
-    parser.add_argument('-v', action='store_true', default=True, help="")
-    parser.add_argument('-c', action='store_true', help="")
-    parser.add_argument('-l', default=60, help="")
+    parser.add_argument('-v', action='store_true', default=True, help="Filter for rosbag where the car moves")
+    parser.add_argument('-c', default=0, help="Filter for rosbags where car does at least X laps")
+    parser.add_argument('-l', default=60, help="Filter for rosbags which are at least x seconds long")
 
     group_file_dir = parser.add_mutually_exclusive_group(required=True)
-    group_file_dir.add_argument('-f', type=str)
-    group_file_dir.add_argument('-r', type=str)
+    group_file_dir.add_argument('-f', type=str, help="Takes an absolute path and loads a single rosbag file (local/NAS)")
+    group_file_dir.add_argument('-r', type=str, help="Takes an absolute path to a directory and loads all rosbags within it be analysed (recursive + local/NAS). WARNING - can be very slow!")
 
     group_nas = parser.add_mutually_exclusive_group(required=False)
-    group_nas.add_argument('--nas-load', action='store_true')
-    group_nas.add_argument('--nas-upload', action='store_true')
+    group_nas.add_argument('--nas-load', action='store_true', help="Load file/directory from the NAS")
+    group_nas.add_argument('--nas-upload', action='store_true', help="Upload filtered rosbags to the NAS")
 
     args = parser.parse_args()
     return args
@@ -41,11 +40,11 @@ def check_module_freq(filename, args):
     for topic_obj in topic_info_dict:
         topic_module = topic_obj["topic"].split(os.path.sep)[1]
         topic_freq = topic_obj["frequency"]
-        if args.per and topic_module is "perception" and topic_freq < 9:
+        if args.per and topic_module == "perception" and topic_freq < 9:
             return_dict["per"] = False
-        if args.est and topic_module is "estimation" and topic_freq < 9: # Adapt! Estimation is edge case! Has messages with extra low frequency!
+        if args.est and topic_module == "estimation" and topic_freq < 9: # Adapt! Estimation is edge case! Has messages with extra low frequency!
             return_dict["est"] = False
-        if args.con and topic_module is "control" and topic_freq < 9:
+        if args.con and topic_module == "control" and topic_freq < 9:
             return_dict["con"] = False
 
     return return_dict
@@ -59,22 +58,28 @@ def ssh_to_nas():
     # TODO: after sshing execute the appropriate commands depending on user input
 
 def check_properties(filename, args):
-    counters_dict = { "vel_counter": 0, "lap_counter": 0 } # Add more counters based on flags
-    msg_topics = ["/can_msgs/velocity_estimation", "/common/lap_counter"] # Add more counters based on flags
+    counters_dict = { "vel_counter": 0, "lap_counter": 0 } # Add more counters if required
+    msg_topics = ["/can_msgs/velocity_estimation", "/common/lap_counter"] # Add more topics if required
 
     # TODO: implement similar logic as for check_module_frequency
     for topic, msg, t in rosbag.Bag(filename, 'r').read_messages(topics=msg_topics):
         msg_dict = message_converter.convert_ros_message_to_dictionary(msg)
 
-        # Increment vel_counter if velocity is message is not 0
-        if topic == "/can_msgs/velocity_estimation" and (msg_dict["vel"]["x"] > 0.0 or msg_dict["vel"]["y"] > 0.0):
+        if args.v and topic == "/can_msgs/velocity_estimation" and (msg_dict["vel"]["x"] > 0.0 or msg_dict["vel"]["y"] > 0.0):
             counters_dict["vel_counter"] += 1
 
-        # Increment lap_counter if lap is message is not 0
-        if topic == "/common/lap_counter" and msg_dict["data"] != 0:
+        if args.c and topic == "/common/lap_counter" and msg_dict["data"] == args.c:
             counters_dict["lap_counter"] += 1
+    
+    return_dict = { "vel": True, "lap": True } # Add more return values if necessary
 
-    print(counters_dict)
+    if counters_dict["vel_counter"] < 50 and args.v:
+        return_dict["vel"] = False
+
+    if counters_dict["lap_counter"] == 0 and args.c:
+        return_dict["lap"] = False
+
+    return return_dict
 
 if __name__ == "__main__":
     args = arg_parser()
