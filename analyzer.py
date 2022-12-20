10292c2bb04601d861ec13032b18c94400df41cb
spiasecki <stanislaw.piasecki@sitrox.com>
2022-12-20 17:58:41 +0100

    Implement initial program logic - analyse_dir_content() and main() functions

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,7 @@
 from rospy_message_converter import message_converter # Used for converting ROS messaged to Python dictionaries
 from pathlib import Path
 from pathlib import PurePath
+from termcolor import colored
 import argparse
 import os
 import rosbag
@@ -12,8 +13,7 @@ def analyzer_arg_parser():
     parser = argparse.ArgumentParser(description="Rosbag Analyzer", epilog="If you have propositions for other flags,\nfeel free to create an issue on GitHub!\nMaintainer: S.Piasecki")
 
     groupFileDir = parser.add_mutually_exclusive_group(required=True)
-    groupFileDir.add_argument('-f', type=str, help="Takes an absolute path to a rosbag to be analysed")
-    groupFileDir.add_argument('-d', type=str, help="Takes an absolute path to a directory and loads all rosbags within it be analysed")
+    groupFileDir.add_argument('-d', type=str, help="Takes an absolute path to a directory and loads all rosbags within it be analysed", default="")
     parser.add_argument('-r', type=str, help="The directory with rosbags is filtered recursively (entire file tree starting at given path is analysed). WARNING - can be very slow!")
 
     parser.add_argument('--nas-load', action='store_true', help="Load files to analyse from the NAS")
@@ -44,11 +44,10 @@ def check_module_freq(filename):
 
     return returnDict
 
-def check_properties(filename):
+def check_rosbag_properties(filename):
     accumulatorDict = { "xVelAvg": 0.0, "yVelAvg": 0.0, "lap": 0 } # Add more accumulators if required
     msgTopics = ["/can_msgs/velocity_estimation", "/common/lap_counter"] # Add more topics if required
 
-    # TODO: implement similar logic as for check_module_frequency
     velMsgCounter = 0
     for topic, msg, t in rosbag.Bag(filename, 'r').read_messages(topics=msgTopics):
         msgDict = message_converter.convert_ros_message_to_dictionary(msg)
@@ -74,6 +73,31 @@ def ssh_to_nas():
     lines = stdout.readlines()
     # TODO: after SSHing execute the appropriate commands depending on user input
 
+def analyse_dir_content(dirName, isRecursive=False):
+    # TODO: create a CSV file, analyse every file in the given directory and save the resulting CSV
+
+    # TODO: remove existing rosbag_analysis.csv files in this directory
+
+    # Analysis
+    for objName in os.listdir(dirName):
+        fullObjPath = dirname + '/' + objName
+        if is_rosbag(fullObjPath): # TODO: implement this function
+            # TODO: analyse the rosbag file
+            # 1) create a rosbag_analysis.csv file if it does not exist
+            # 2) add row corresponding to CSV
+            # 3) inform the user via standard output that file was succesfully analysed
+        elif os.path.isdir(fullObjPath) and isRecursive:
+            analyse_dir_content(fullObjPath, isRecursive)
+        else:
+            print(colored(f'Ignoring {fullObjPath}: it is not a rosbag or directory!', 'cyan'))
+
+def main(args):
+    # Check the path which user gave as input
+    assert not args.d == "", f"The script requires the path of the directory to analyse as parameter!"
+    assert os.path.isdir(args.d), f"The passed directory does not exist!"
+
+    analyse_dir_content(args.d, args.r)
+
 if __name__ == "__main__":
-    # TODO: call main function once it is implemented
-    check_module_freq("autocross_2022-08-12-09-04-24.bag.active")
+    args = analyzer_arg_parser()
+    main(args)
